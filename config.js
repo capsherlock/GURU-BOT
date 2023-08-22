882c47f6c0f5184ffe6168c17564ea2d0f465189
capsherlock <141975448+capsherlock@users.noreply.github.com>
2023-08-22 16:36:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['91562331013', 'SHERLOCK, true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
 
 global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
+global.prems = ['917605902011', '32460248586', '919398758484',]
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -49,9 +49,9 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
+global.packname = 'Ƈαρƚαιɳ𓆩🖤𓆪ᏕᎻᎬᏒᏝᎾᏨᏦ⇝ 
+global.author = '-𝐂𝐒-' 
+global.igfg = '▢ Follow on Instagram/https://www.instagram.com/yaseen_yasz/#
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
 global.fgyt = 'https://youtube.com/@Asliguru'
